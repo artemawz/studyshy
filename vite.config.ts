632c70675767481e88d7db6f460bbdbf40ece9a1
Felix Schrage <artemawz@pm.me>
2026-07-01 09:28:06 +0200

    refactor(config): removed vue dev-tools

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
@@ -10,7 +9,7 @@ export default defineConfig(({ mode }) => {
   const apiTarget = env.VITE_API_PROXY_TARGET ?? 'http://localhost:8000'
 
   return {
-    plugins: [vue(), vueDevTools()],
+    plugins: [vue()],
     resolve: {
       alias: {
         '@': fileURLToPath(new URL('./src', import.meta.url)),
